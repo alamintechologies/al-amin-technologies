b5243fc56dd5eaf7798dd657fbe7b0db0c892ca0
Danish <danish@pop-os.domain.name>
2026-05-22 00:32:48 +0530

    checking ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const navLinks = [
   { label: "Services", href: "#services" },
   { label: "Why Us", href: "#why-us" },
   { label: "Projects", href: "#projects" },
-  { label: "Contacts", href: "#contact" },
+  { label: "Contacts123343", href: "#contact" },
 ]
 
 const Navbar = () => {
