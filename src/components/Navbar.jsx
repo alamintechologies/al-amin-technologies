6e3ab9a36b04e6c2312c3c696bc18ca18e9918cf
Danish <danish@pop-os.domain.name>
2026-05-22 00:31:44 +0530

    staged

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const navLinks = [
   { label: "Services", href: "#services" },
   { label: "Why Us", href: "#why-us" },
   { label: "Projects", href: "#projects" },
-  { label: "Contactsssss", href: "#contact" },
+  { label: "Contacts", href: "#contact" },
 ]
 
 const Navbar = () => {
